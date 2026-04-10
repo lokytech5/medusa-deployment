8b91ef9ba642df3ba4431c67f3cab6706a211785
lokytech5 <lokytech5@gmail.com>
2026-04-10 10:58:04 +0100

    update task-defiintion file to use ecr data block filed which means am using exiting ecr created in the console and should not be manage by terraform

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "medusa_ecr_repository_url" {
-  value = aws_ecr_repository.medusa_app_repository.repository_url
+  value = data.aws_ecr_repository.medusa_app_repository.repository_url
 }
 
 output "medusa_ecs_task_execution_role_arn" {
